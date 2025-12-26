b7d4f746d06fcf34cd46130fd275740187ebd61f
SerSu <sersurgut88@yandex.ru>
2025-12-26 23:05:26 +0300

    Complete task 1

--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
 
+export default function FilterCafes() {
+  return (
+    <div className="controls">
+      <select name="subway" id="subway">
+        <option value="All">Все</option>
+      </select>
+    </div>
+  );
+};
